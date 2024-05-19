e33b8549143f5aaf96367f764aa0f8c62cdbd2f8
Subhasish Saha <subhasishsaha007@gmail.com>
2024-05-19 08:50:09 +0530

    Added : Setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative_AI_Project',
+    version= '0.0.0',
+    author= 'Subhasish',
+    author_email= 'subhasishsaha007@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
