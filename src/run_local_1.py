a528e4df050a03f238311142e6f3ba3c3c243d8e
Subhasish Saha <subhasishsaha007@gmail.com>
2024-05-19 08:51:18 +0530

    Added : Pipeline testing

--- src/run_local_1.py
+++ src/run_local_1.py
@@ -7,9 +7,10 @@ B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
 
-instruction = "Convert the following text  from English to Bengali : \n\n {text}"
+# instruction = "Convert the following text  from English to Bengali : \n\n {text}"
+instruction = "Give a proper summary of the of : \n\n {text}"
 
-SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+SYSTEM_PROMPT = B_SYS + CUSTOM_SYSTEM_PROMPT + E_SYS
 template = B_INST + SYSTEM_PROMPT + instruction + E_INST
 
 prompt = PromptTemplate(template=template,input_variables=["text"])
@@ -23,4 +24,4 @@ llm = CTransformers(model='model/llama-2-7b-chat.ggmlv3.q2_K.bin',
 
 LLM_Chain=LLMChain(prompt=prompt, llm=llm)
 
-print(LLM_Chain.run("How are you?"))
+print(LLM_Chain.run("Lord of the Rings"))
